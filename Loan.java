c3c2c8446b696254f311f0ecb337bf26b8297f9b
Jorge Murilo <jorge.murilo@sempreceub.com>
2025-12-04 16:06:28 -0300

    Create Loan.java

---
+++ Loan.java
@@ -0,0 +1,39 @@
+package br.com.biblioteca.model.entities;
+
+import java.time.LocalDate;
+
+public class Loan {
+    private int id;
+    private int bookId;
+    private int memberId;
+    private LocalDate loanDate;
+    private LocalDate dueDate;
+    private LocalDate returnDate;
+
+    public Loan() {}
+
+    public Loan(int id, int bookId, int memberId, LocalDate loanDate, LocalDate dueDate) {
+        this.id = id; this.bookId = bookId; this.memberId = memberId;
+        this.loanDate = loanDate; this.dueDate = dueDate;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getBookId() { return bookId; }
+    public void setBookId(int bookId) { this.bookId = bookId; }
+
+    public int getMemberId() { return memberId; }
+    public void setMemberId(int memberId) { this.memberId = memberId; }
+
+    public LocalDate getLoanDate() { return loanDate; }
+    public void setLoanDate(LocalDate loanDate) { this.loanDate = loanDate; }
+
+    public LocalDate getDueDate() { return dueDate; }
+    public void setDueDate(LocalDate dueDate) { this.dueDate = dueDate; }
+
+    public LocalDate getReturnDate() { return returnDate; }
+    public void setReturnDate(LocalDate returnDate) { this.returnDate = returnDate; }
+
+    public boolean isReturned() { return returnDate != null; }
+}
